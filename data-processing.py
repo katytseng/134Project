76c1acf058dd138087c50a733a372a73dfc8e8c5
aq2oo3 <hetarepianist@gmail.com>
2025-05-14 19:59:00 -0700

    weighted score

--- data-processing.py
+++ data-processing.py
@@ -1,8 +1,24 @@
+# https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
+
 import pandas as pd
-import numpy as np
+import math
+# import numpy as np
 
-yelp_df = pd.read_json('Data/Yelp JSON/yelp_dataset/yelp_academic_dataset_business.json')
+yelp_df = pd.read_json('Data/Yelp JSON/business.json', lines=True)
 
-print(yelp_df.head(5), lines=True)
+yelp_df = yelp_df[['name', 'city', 'state', 'stars', 'review_count']]
 
-# hi
+yelp_10 = yelp_df.head(10)
+
+def weighted_sort(score, reviews):
+    weight_score = score - (score - 0.5) * (2 ** -math.log10(reviews + 1))
+    return weight_score
+
+yelp_10['w_score'] = yelp_10.apply(lambda x: weighted_sort(x.stars, x.review_count), axis=1)
+
+
+#print(yelp_df.head(10))
+#print(yelp_df.info(verbose=True))
+
+# print(yelp_df.loc[:, "city"])
+print(yelp_10)
