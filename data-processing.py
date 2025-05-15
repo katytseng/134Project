5c33a5e3203a43a74f1e67850f402d9a8cc73550
aq2oo3 <hetarepianist@gmail.com>
2025-05-14 21:08:06 -0700

    pls

--- data-processing.py
+++ data-processing.py
@@ -1,7 +1,9 @@
 # https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
+# https://steamdb.info/blog/steamdb-rating/
 
 import pandas as pd
 import math
+from scipy.stats import norm
 # import numpy as np
 
 yelp_df = pd.read_json('Data/Yelp JSON/business.json', lines=True)
@@ -10,11 +12,11 @@ yelp_df = yelp_df[['name', 'city', 'state', 'stars', 'review_count']]
 
 yelp_10 = yelp_df.head(10)
 
-def weighted_sort(score, reviews):
+def steam_weighted_sort(score, reviews):
     weight_score = score - (score - 0.5) * (2 ** -math.log10(reviews + 1))
     return weight_score
 
-yelp_10['w_score'] = yelp_10.apply(lambda x: weighted_sort(x.stars, x.review_count), axis=1)
+yelp_10['w_score'] = yelp_10.apply(lambda x: steam_weighted_sort(x.stars, x.review_count), axis=1)
 
 
 #print(yelp_df.head(10))
